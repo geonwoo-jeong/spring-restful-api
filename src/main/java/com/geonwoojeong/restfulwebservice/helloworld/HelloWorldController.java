d9fc58442108f7fc553b113cceaa01e2ea352870
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-03-07 22:08:11 +0900

    Add Internationalization

--- src/main/java/com/geonwoojeong/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/geonwoojeong/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.geonwoojeong.restfulwebservice.helloworld;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
+
+import java.util.Locale;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloWorldController {
+
+  private final LocaleResolver localeResolver;
+  private final MessageSource messageSource;
 
   @GetMapping("/hello-world")
   public String helloWorld() {
@@ -15,5 +25,11 @@ public class HelloWorldController {
   @GetMapping("/hello-world-bean/path-variable/{name}")
   public HelloWorldBean helloWorldBean(@PathVariable String name) {
     return new HelloWorldBean(String.format("Hello World, %s", name));
+  }
+
+  @GetMapping("/hello-world-internationalized")
+  public String helloWorldInternationalized(
+          @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+    return messageSource.getMessage("greeting.message", null, locale);
   }
 }
