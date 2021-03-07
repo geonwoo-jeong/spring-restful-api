cd27f0afb2d9fc4e893076f5015e93a3d0c428a7
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-03-07 20:54:39 +0900

    Control HTTP Status Code

--- src/main/java/com/geonwoojeong/restfulwebservice/user/UserController.java
+++ src/main/java/com/geonwoojeong/restfulwebservice/user/UserController.java
@@ -1,12 +1,15 @@
 package com.geonwoojeong.restfulwebservice.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -25,7 +28,14 @@ public class UserController {
   }
 
   @PostMapping("/users")
-  public void createUser(@RequestBody User user) {
+  public ResponseEntity<User> createUser(@RequestBody User user) {
     User savedUser = userDaoService.save(user);
+
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(savedUser.getId())
+            .toUri();
+
+    return ResponseEntity.created(location).build();
   }
 }
