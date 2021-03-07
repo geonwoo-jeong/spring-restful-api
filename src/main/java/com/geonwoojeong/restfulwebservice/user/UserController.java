f0534f67cf096f449e2d061362c7c2ab31f40fc8
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-03-07 20:49:44 +0900

    Add User Add (Post Method)

--- src/main/java/com/geonwoojeong/restfulwebservice/user/UserController.java
+++ src/main/java/com/geonwoojeong/restfulwebservice/user/UserController.java
@@ -3,6 +3,8 @@ package com.geonwoojeong.restfulwebservice.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +22,10 @@ public class UserController {
   @GetMapping("/users/{id}")
   public User retrieveUser(@PathVariable int id) {
     return userDaoService.findOne(id);
+  }
+
+  @PostMapping("/users")
+  public void createUser(@RequestBody User user) {
+    User savedUser = userDaoService.save(user);
   }
 }
