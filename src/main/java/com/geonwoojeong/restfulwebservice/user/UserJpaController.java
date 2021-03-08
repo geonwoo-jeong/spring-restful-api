65f42d150766a975861d980b2fe9561f1e4df939
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-03-08 17:06:05 +0900

    Add Post list

--- src/main/java/com/geonwoojeong/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/geonwoojeong/restfulwebservice/user/UserJpaController.java
@@ -39,4 +39,15 @@ public class UserJpaController {
 
     return user.get();
   }
+
+  @GetMapping("/users/{id}/posts")
+  public List<Post> retrieveAllPostsByUser(@PathVariable int id) {
+    Optional<User> user = userRepository.findById(id);
+
+    if (!user.isPresent()) {
+      throw new UserNotFoundException(String.format("ID[%s} not found", id));
+    }
+
+    return user.get().getPosts();
+  }
 }
